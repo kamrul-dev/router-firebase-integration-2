51cc259d1e8f0eb40f3f8b90a23e4ca14b531a2a
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-09 16:45:04 +0600

    text correction for register link

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
                 <Link to='/'>Home</Link>
                 <Link to='/products'>Products</Link>
                 <Link to='/orders'>Orders</Link>
-                <Link to='/register'>Rigister</Link>
+                <Link to='/register'>Register</Link>
                 <Link to='/login'>Login</Link>
             </nav>
         </div>

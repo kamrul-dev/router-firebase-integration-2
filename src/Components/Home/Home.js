87c2bf193fdfaa423dac15bb846097e515435f57
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-09 19:17:04 +0600

    user info display in home and products page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,16 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import app from '../../Firebase/Firebase';
+
+const auth = getAuth(app);
 
 const Home = () => {
+    const [user] = useAuthState(auth);
     return (
         <div>
             <h2>This is Home Page !!!</h2>
+            <h2>User Found: {user?.displayName ? user.displayName : 'User Not Signed in!!'}</h2>
         </div>
     );
 };

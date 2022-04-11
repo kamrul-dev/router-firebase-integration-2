e3c16b04386f7b62fbeffa70933aa70f1d9a69d9
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-11 17:14:45 +0600

    RquireAuth implemented

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Products from './Components/Products/Products';
 import Orders from './Components/Orders/Orders';
 import Register from './Components/Register/Register';
 import Login from './Components/Login/Login';
+import RequireAuth from './Components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -15,7 +16,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/products' element={<Products></Products>}></Route>
-        <Route path='/orders' element={<Orders></Orders>}></Route>
+        <Route path='/orders' element={
+          <RequireAuth>
+            <Orders></Orders>
+          </RequireAuth>
+        }></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,15 +3,24 @@ import app from '../../Firebase/Firebase';
 import './Login.css';
 import { getAuth } from 'firebase/auth';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 const auth = getAuth(app);
 
 const Login = () => {
     const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || "/"
+
 
     const handleSignInWithGoogle = () => {
         signInWithGoogle()
+        .then(() => {
+            navigate(from, {replace: true});
+        })
     }
 
     return (
